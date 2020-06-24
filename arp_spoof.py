78d47cf2012ea7c5f94fb74907456a7f6b29dc13
Nadula Gardiyehewa <51786507+NadulaG@users.noreply.github.com>
2020-06-24 12:20:35 -0400

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -23,8 +23,8 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
 
-target_ip = "10.0.2.15"
-gateway_ip = "10.0.2.1"
+target_ip = "xxxxx"
+gateway_ip = "xxxxx"
 
 try:
     sent_packets_count = 0
